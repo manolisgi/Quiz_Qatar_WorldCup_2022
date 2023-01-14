f0dee0c117e5a05b9627eca6c8065a4b405c74a0
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-01-14 10:34:40 +0000

    added the first 3 questions

---
+++ index.js
@@ -0,0 +1,10 @@
+let questions = [{question: "Which nation was the first Asian country to reach the World Cup semi-finals?", answers: ["South Korea", "India", "Japan", "China"], correctAnswer: 0}, 
+{question: "Who is the all-time leading World Cup goalscorer?", answers: ["Miroslav Klose", "Cristiano Ronaldo", "Just Fontaine", "Gary Lineker"], correctAnswer: 0}, 
+{question: "Which national team had the most completed passes?", answers: ["Belgium", "Argentina", "Brazil", "Cameroon"], correctAnswer: 1}
+
+
+
+]
+
+
+let currentQuestion = 1;
